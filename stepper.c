418e4e2390c03ef885f27e8b5b8ad0fe4663d162
Rob Reilink <rob@reilink.net>
2017-12-25 09:53:02 +0100

    Added documentation; bit of clean-up

--- stepper.c
+++ stepper.c
@@ -1,3 +1,43 @@
+/*
+ This file implements the core motion algorithms. It consists mainly
+ of two parts: the step-generator and the velocity controller.
+ 
+ The step-generator and the velocity controller share a 'block-buffer'
+ which contains blocks (straight-line sections) to be executed. The
+ buffer is implemented as a ring-buffer. The 'tail' (the oldest item
+ in the buffer) is the one that is currently being executed by the
+ step-generator. Each block prescribes the number of steps for each
+ stepper motor, as well as the maximum speed during the block and at
+ the end of the block.
+ 
+ The step-generator is driven by the ARM timer interrupt, and runs in
+ the ARM fast interrupt request(FIQ) interrupt handler. The FIQ
+ interrupt gives it the highest priority in the system (FIQ can
+ interrupt 'normal' INT interrupts, and FIQ should never be disabled
+ while INT could sometimes be disabled at critical sections). The
+ step-generator mainly does one thing: at every timer tick, it
+ determines which steppers to advance, and generates the required tick
+ pulses. It does this using a Bresenham algorithm. When a block is
+ completed, the step-generator fetches the next block from the block
+ buffer (discarding the old one). Note that the last block in the buffer
+ is never discarded, i.e. there is always a 'current' block.
+ 
+ The velocity controller runs at a (usually lower) fixed rate, driven
+ by the app_systick() hook. Every system tick, it looks ahead through
+ the block buffer, determining the maximum current speed such that none
+ of the maximum speeds prescribed by the oncoming blocks will be
+ violated (given the maximum acceleration). It adjusts the tick
+ frequency of the ARM timer which drives the step-generator, thereby
+ controlling the speed of the steppers.
+ 
+ In its current implementation, the velocity controller only deals with
+ one common acceleration value (in steps/s^2) for all axis.
+
+
+
+ */
+
+
 #include "ch.h"
 #include "hal.h"
 #include "bcm2835.h"
@@ -6,6 +46,7 @@
 
 #define MAX_AXIS 8
 #define BUFFER_SIZE 32
+
 
 
 typedef struct {
@@ -72,23 +113,13 @@ static inline unsigned int buffer_next_index(unsigned int index) {
     return index;
 }
 
-static inline unsigned int buffer_prev_index(unsigned int index) {
-    if (index == 0) index = BUFFER_SIZE;
-    return index-1;
-}
-
-
-static inline int buffer_empty(void) {
-    return buffer_head == buffer_tail;
-}
-
 
 /*
-    Copy a new block into the buffer
+ Copy a new block into the buffer
     
-    Returns 0 on success, -1 on error (buffer full)
-    This is the only place where we modify buffer_head 
- */
+ Returns 0 on success, -1 on error (buffer full)
+ This is the only place where buffer_head is modified
+*/
 int stepper_put_buffer(stepper_block_t *block) {
     unsigned int next_head = buffer_next_index(buffer_head);
 
@@ -102,14 +133,24 @@ int stepper_put_buffer(stepper_block_t *block) {
 }
 
 
+/*
+ Current state of the stepper algorithm
+ 
+*/
 
 typedef struct {
     stepper_block_t *current_block;
-    int32_t accu[MAX_AXIS];
+    int32_t accu[MAX_AXIS];         //Bresenham accumulator
+    
+    uint32_t all_step_mask; // binary OR of all step_masks; initialized by stepper_init
+    uint32_t all_dir_mask;  // binary OR of all dir_masks; initialized by stepper_init
 } stepper_status_t;
 
-stepper_status_t st;
+static stepper_status_t st;
 
+/*
+ Hardware configuration of the machine: I/O mapping of the axes
+*/
 typedef struct {
     int naxis;
     uint32_t step_mask[MAX_AXIS];
@@ -123,15 +164,14 @@ stepper_config_t stepper_config = {
 };
 
 
-static uint32_t all_step_mask; // initialized by stepper_init
-static uint32_t all_dir_mask; // initialized by stepper_init
+
 
 static const float dt = 0.001f;
 
-static const float vmax = 80.0f * 0.001f;              // mm/timestep
-static const float steps_per_mm = (32*400.0f/40.0f);
-static const float mm_per_step = (40.0f/(32*400.0f));
-static const float amax = 100.0f  * 0.001f * 0.001f;         // mm/(timestep^2)
+static float vmax = 80.0f * 0.001f;              // mm/timestep
+static float steps_per_mm = (32*400.0f/40.0f);
+static float mm_per_step = (40.0f/(32*400.0f));
+static float amax = 100.0f  * 0.001f * 0.001f;         // mm/(timestep^2)
 float current_speed;
 unsigned int test_ctr = 0;
 
@@ -156,7 +196,7 @@ void stepper_init(void) {
         block.vmax_sq = vmax*vmax;
         block.vmax_end_sq = vmax*vmax;
         stepper_put_buffer(&block);
-        
+        if (i == 1) break;
         block.direction_bits = 0;
         block.steps[0] = 200;
         block.steps[1] = 0;
@@ -192,19 +232,20 @@ void stepper_init(void) {
     
 
     /* Compute all_step_mask and all_dir_mask, they are the binary OR of all
-       step_masks and dir_masks, respectively 
+       step_masks and dir_masks, respectively. These are precomputed for use
+       by the step-generator.
     */
-    all_step_mask=0;
-    all_dir_mask=0;
+    st.all_step_mask=0;
+    st.all_dir_mask=0;
     for (int i=0; i<stepper_config.naxis; i++) {
-        all_step_mask |= stepper_config.step_mask[i];
-        all_dir_mask |= stepper_config.dir_mask[i];
+        st.all_step_mask |= stepper_config.step_mask[i];
+        st.all_dir_mask |= stepper_config.dir_mask[i];
     }
     
     
-    GPCLR0 = all_step_mask | all_dir_mask;
+    GPCLR0 = st.all_step_mask | st.all_dir_mask;
     
-    palSetGroupMode(&IOPORT0, all_step_mask | all_dir_mask, 0, PAL_MODE_OUTPUT);
+    palSetGroupMode(&IOPORT0, st.all_step_mask | st.all_dir_mask, 0, PAL_MODE_OUTPUT);
     
     ARM_TIMER_CTL = 0x003E0000;
     ARM_TIMER_LOD = 1000-1;
@@ -229,11 +270,27 @@ static inline float limit(float x, float y) { return (y<x) ? y : x; }
 
 static inline float sq(float x) { return x*x; }
 
+
+/*
+ Single-pass online velocity planner
+ 
+ Default: accelerate upto maximum velocity of the current block.
+ 
+ Then, for each subsequent block, compute the accumulated distance from
+ the present position. Determine the maximum current velocity such that
+ decelleration downto the maximum velocity at each position is possible.
+ Limit the current velocity to the minimum of those.
+ 
+ Several computations are done using the squared value of parameters.
+ These are denoted by the suffix _sq . This is done to reduce the number
+ of computationally expensive square root calculations.
+ 
+*/
+
 void app_systick(void) {
-    // one-pass online velocity planner
+
     unsigned int my_buffer_tail;
     stepper_block_t *bl;
-    float new_speed_sq;
     
     my_buffer_tail = buffer_tail; //atomic copy
     /* FIQ interrupt could update buffer_tail, but data will remain valid
@@ -243,12 +300,14 @@ void app_systick(void) {
        can be changed by FIQ while we are processing
      */
 
-    int cumulative_steps = 0;
-    float speed_limit_sq;
+    int cumulative_steps = 0; // Total steps from present position upto point
+    float new_speed_sq;       // Lowest speed limit found so far
+    float speed_limit_sq;     // Temporary: calculation of a speed limit for a given section
     
     // default: maximum acceleration
     new_speed_sq = sq(current_speed + amax);
 
+    // Find the current block, and the number of steps still to go in that block
     bl = &buffer[my_buffer_tail];
     cumulative_steps = bl->total_steps - bl->step_position;
     
@@ -256,11 +315,11 @@ void app_systick(void) {
     new_speed_sq = limit(new_speed_sq, bl->vmax_sq);
     
     // compute max current speed in order to be able to decellerate
-    // to bl_vmax_end_sq before reaching end of this block
+    // to bl->vmax_end_sq before reaching end of this block
     speed_limit_sq = 2*amax*cumulative_steps*mm_per_step + bl->vmax_end_sq;
-    
     new_speed_sq = limit(new_speed_sq, speed_limit_sq);
     
+    // Loop through the buffer from current to the future
     unsigned int i;
     i = buffer_next_index(my_buffer_tail);
     
@@ -277,6 +336,12 @@ void app_systick(void) {
         i=buffer_next_index(i);
     }
     
+    // be prepared to stop at the end of the buffer
+    speed_limit_sq = 2*amax*cumulative_steps*mm_per_step;
+    new_speed_sq = limit(new_speed_sq, speed_limit_sq);    
+    
+    
+    // Compute the timer reload value for the found speed
     uint32_t reload_value;
     current_speed = sqrtf(new_speed_sq); // mm per dt
 
@@ -286,15 +351,15 @@ void app_systick(void) {
         reload_value = (1000*mm_per_step) / current_speed;
         if (reload_value>10000) reload_value = 10000; //minimum 100 steps/s
     }
-    //mini_uart_sendhex((int)(current_speed*1000000), 1);
-    ARM_TIMER_RLD = reload_value;
     
-    test_ctr++;
+    ARM_TIMER_RLD = reload_value;
 }
 
 
 __attribute__((naked)) 
 void FiqHandler(void) {
+    // FIQ handler init code; push all registers that are shared with other
+    // modes than FIQ mode
     asm volatile ("stmfd    sp!, {r0-r7, r12, lr}" : : : "memory");
     dmb();
     
@@ -302,13 +367,10 @@ void FiqHandler(void) {
     stepper_block_t *bl;
     
     // All step signals low
-    GPCLR0 = all_step_mask;
-    step_mask = BIT(22)| BIT(24);
+    GPCLR0 = st.all_step_mask;
     
+    // Get next block, if required
     bl =  st.current_block;
-
-
-    
     if (bl->step_position == bl->total_steps) {
         unsigned int next_tail = buffer_next_index(buffer_tail);
         if (next_tail == buffer_head) {
@@ -323,12 +385,11 @@ void FiqHandler(void) {
             }
         }
     }
-    
-    
-    // Set direction signals
-    GPCLR0 = all_dir_mask & ~bl->direction_bits;    
-    GPSET0 = bl->direction_bits;
 
+    // Set direction signals
+    GPCLR0 = st.all_dir_mask & ~bl->direction_bits;    
+    GPSET0 = bl->direction_bits;
+    
     // Determine which axes to step
     step_mask = 0;    
     if (bl->step_position == bl->total_steps) { 
@@ -353,6 +414,9 @@ void FiqHandler(void) {
     ARM_TIMER_CLI = 0;
     dmb();
     GPSET0 = step_mask; // generate step pulse
+    
+    
+    // FIQ handler exit code
     dmb();
     asm volatile ("ldmfd    sp!, {r0-r7, r12, lr}" : : : "memory");    
     asm volatile ("subs    pc, lr, #4" : : : "memory");    
